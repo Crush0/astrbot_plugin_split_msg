2c905c2afd57e9f789573c190e58afb28d64e854
crush0 <2925663480@qq.com>
2025-12-05 20:17:31 +0800

    refactor: 重构消息处理逻辑以支持消息链分段
    将单一文本消息处理改为支持消息链处理，能够正确分段处理纯文本消息并保持其他消息类型不变。移除不再使用的代码注释。

--- main.py
+++ main.py
@@ -9,28 +9,17 @@ class MyPlugin(Star):
         self.cn_segmenter = Segmenter(language="zh", clean=False)
         self.en_segmenter = Segmenter(language="en", clean=False)
 
-    # async def initialize(self):
-    #     """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
-
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    # @filter.command("helloworld")
-    # async def helloworld(self, event: AstrMessageEvent):
-    #     """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-    #     user_name = event.get_sender_name()
-    #     message_str = event.message_str # 用户发的纯文本消息字符串
-    #     message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-    #     logger.info(message_chain)
-    #     yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
     @filter.on_decorating_result()
     async def on_decorating_result(self, event: AstrMessageEvent):
-        msg = event.message_str
-        # 如果msg中包含a-z或A-Z 使用英文分句器
-        if any(c.isalpha() for c in msg):
-            sentences = self.en_segmenter.segment(msg)
-        else:
-            sentences = self.cn_segmenter.segment(msg)
-        for sentence in sentences:
-            event.send(Plain(sentence))
+        msgs = event.get_messages()
+        result = []
+        for msg in msgs:
+            if isinstance(msg, Plain):
+                result.extend([MessageChain([Plain(text)]) for text in self.cn_segmenter.segment(msg.text)])
+            else:
+                result.append(msg)
+        for i in result:
+            await event.send(i)
         event.stop_event()
 
     # async def terminate(self):
