dc72584b5b47d0faa7019ee46af5e87b590802d1
crush0 <2925663480@qq.com>
2025-12-05 20:21:07 +0800

    feat: 添加 MessageChain 导入以支持消息链功能

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
 from astrbot.api.message_components import Plain
 from pysbd import Segmenter
+from astrbot.core.message.message_event_result import MessageChain
 @register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
 class MyPlugin(Star):
     def __init__(self, context: Context):
