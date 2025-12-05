1ca2dfe708ced97898d371722406cbea47eeaac0
crush0 <2925663480@qq.com>
2025-12-06 00:11:16 +0800

    feat: 添加消息分块插件功能
    实现消息分块功能，根据配置的阈值对长消息进行分割。新增配置项 words_count_threshold 控制分块阈值，默认值为15。修改插件名称和描述，更新版本号至v1.5.0。

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
 from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
 from astrbot.api.message_components import Plain
+from astrbot.api import AstrBotConfig
 from pysbd import Segmenter
-from astrbot.core.message.message_event_result import MessageChain
 @register("split_msg", "Crush0", "一个简单的消息分割插件", "1.0.5")
 class MyPlugin(Star):
-    def __init__(self, context: Context):
+    def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
+        self.ctx = context
+        self.config = config
+        self.words_count_threshold = self.config.get("words_count_threshold", 15)
         self.cn_segmenter = Segmenter(language="zh", clean=False)
         self.en_segmenter = Segmenter(language="en", clean=False)
 
     @filter.on_decorating_result()
     async def on_decorating_result(self, event: AstrMessageEvent):
-        msgs = event.get_messages()
-        result = []
-        for msg in msgs:
-            if isinstance(msg, Plain):
-                result.extend([MessageChain([Plain(text)]) for text in self.cn_segmenter.segment(msg.text)])
-            else:
-                result.append(MessageChain([msg]))
-        for i in result:
-            await event.send(i)
-        event.stop_event()
+        result = event.get_result()
+        if result is None:
+            return
+        if len(result.chain) > 0:
+            if event.get_platform_name() not in [
+                "qq_official",
+                "weixin_official_account",
+                "dingtalk",
+            ]:
+                new_chain = []
+                for comp in result.chain:
+                    if len(comp.text) < self.words_count_threshold:
+                        # 不分段回复
+                        new_chain.append(comp)
+                        continue
+                    if not comp.text.isascii():
+                        split_response = self.cn_segmenter.segment(comp.text)
+                    else:
+                        split_response = self.en_segmenter.segment(comp.text)
+                    new_chain.extend([Plain(text) for text in split_response if text.strip()])
+                result.chain = new_chain
+                event.set_result(result)
 
     # async def terminate(self):
     #     """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
